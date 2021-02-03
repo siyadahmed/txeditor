c54806a715cab004b411097131dc939e778509d3
siyadahmed <siyadahmed@users.noreply.github.com>
2021-02-03 15:57:03 +0530

    commented JSHINT import

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import {JSHINT} from 'jshint';
+//import {JSHINT} from 'jshint';
 
 if (environment.production) {
   enableProdMode();
